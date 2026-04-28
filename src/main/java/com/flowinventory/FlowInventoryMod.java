607233135f1986d7f01f43409fedeee6f81581be
Abdelhakim-baalla <abdelhakimbaalla50@gmail.com>
2026-04-28 04:11:48 +0100

    feat: connect FlowConfig to mod entry point

--- src/main/java/com/flowinventory/FlowInventoryMod.java
+++ src/main/java/com/flowinventory/FlowInventoryMod.java
@@ -1,5 +1,6 @@
 package com.flowinventory;
 
+import com.flowinventory.config.FlowConfig;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,8 +10,11 @@ public class FlowInventoryMod implements ModInitializer {
     public static final String MOD_ID = "flowinventory";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+    public static FlowConfig config;
+
     @Override
     public void onInitialize() {
-        LOGGER.info("[FlowInventory] Mod loaded successfully!");
+        config = FlowConfig.load();
+        LOGGER.info("[FlowInventory] Mod loaded! Auto-detect: " + config.autoDetectActivity);
     }
 }
